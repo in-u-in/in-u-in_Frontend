e7261d27f61b1b1a7277c2fbeb8b2054d272c3dd
LeeJooyeon <smilejuju@ewha.ac.kr>
2024-08-02 23:24:58 +0900

    feat: router 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,10 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import FounderHomePage from "./founder-home";
-import IntervieweeHomePage from "./interviewee-home";
-import CreateInterviewPage from "./create-interview";
 import "./styles/globals.css"
 
 
 const App = () => {
   return (
     <div>
-      <BrowserRouter>
-        <Routes>
-            <Route path="/founderhome" element={<FounderHomePage />} />
-            <Route path="/intervieweehome" element={<IntervieweeHomePage/>}/>
-            <Route path="/createinterview" element={<CreateInterviewPage/>}/>
-        </Routes>
-      </BrowserRouter>
+      <h1>hello world!</h1>
     </div>
   );
 };
