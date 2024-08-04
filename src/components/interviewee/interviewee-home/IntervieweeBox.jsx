7835f0386ad458ac6663efd75e15d1f76022f298
sujinRo <88073842+sujinRo@users.noreply.github.com>
2024-08-04 14:03:41 +0900

    refactor: assets 폴더 위치 변경

--- src/components/interviewee/interviewee-home/IntervieweeBox.jsx
+++ src/components/interviewee/interviewee-home/IntervieweeBox.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './styles/IntervieweeBox.module.css';
-import tourism from './tourism.jpg';
+import tourism from '../../../assets/icons/tourism.svg';
 
 export default function IntervieweeBox() {
   return (
