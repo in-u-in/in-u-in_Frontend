1d9087c722b46517580499136e711405c79c511a
Jinho622 <aceho21@gmail.com>
2024-08-16 22:27:16 +0900

    feat #8 랜딩 페이지 일곱번째 페이지 완성, 네번째 페이지 기능 완성

---
+++ src/components/home/Box1Cliked.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import styles from '../../styles/home.module.css';
+
+export default function Box1Cliked() {
+  return (
+    <div class={styles.container_page4_box1_cliked}>
+      <div className={styles.container_page4_title}>
+        <h3>체험을 통해 경험해보세요!</h3>
+      </div>
+      <div className={styles.container_page4_detail_price}> </div>
+      <div className={styles.container_page4_description1_1}>
+        <ul>
+          <li> 정성적 인터뷰 관련 전자책 증정</li>
+          <li> 정성적 인터뷰 관련 컨설팅 1회</li>
+        </ul>
+      </div>
+      <div className={styles.container_page4_description2}>
+        <h3>
+          매일 오전 11시 & 오후 5시마다
+          <br />
+          무료 인터뷰 매칭 후보 업데이트
+        </h3>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/home/Box2Cliked.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import styles from '../../styles/home.module.css';
+
+export default function Box2Cliked() {
+  return (
+    <div class={styles.container_page4_box2_cliked}>
+      <div className={styles.container_page4_title}>
+        <h3>구독을 통해 경험해보세요!</h3>
+      </div>
+      <div className={styles.container_page4_detail_price}>50000/월</div>
+      <div className={styles.container_page4_description1}>
+        <ul>
+          <li> 정성적 인터뷰 자동 기록 가능</li>
+          <li> 인터뷰 분석 노트 3개월마다 제공</li>
+        </ul>
+      </div>
+      <div className={styles.container_page4_description2}>
+        <h3>
+          매일 오전 11시 & 오후 5시마다
+          <br />
+          무료 인터뷰 매칭 후보 업데이트
+        </h3>
+      </div>
+    </div>
+  );
+}
