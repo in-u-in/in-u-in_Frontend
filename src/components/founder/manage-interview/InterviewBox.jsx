989d8744c8c908d83296280402b432ed0d39d1fe
sujinRo <88073842+sujinRo@users.noreply.github.com>
2024-08-30 18:53:11 +0900

    feat: #14 관리 페이지 내 swiper 기능 / interview에 맞는 interviewee 나오는 기능

--- src/components/founder/manage-interview/InterviewBox.jsx
+++ src/components/founder/manage-interview/InterviewBox.jsx
@@ -18,26 +18,53 @@ export default function InterviewBox({
   return (
     <Link
       to={`/manageInterview/manageInterviewee/${id}`}
-      className={styles.interviewBoxContainer}
+      className={`${type == 'big' ? styles.bigInterviewBoxContainer : type == 'mid' ? styles.interviewBoxContainer : styles.smallInterviewBoxContainer}`}
     >
-      <div className={styles.topBox} />
-      <div className={styles.interviewBoxTitle}>{title}</div>
-      <div className={`${styles.details} ${styles.first_details}`}>
+      <div
+        className={`${type == 'big' ? styles.bigTopBox : type == 'mid' ? styles.topBox : styles.smallTopBox}`}
+      />
+      <div
+        className={`${type == 'big' ? styles.bigInterviewBoxTitle : type == 'mid' ? styles.interviewBoxTitle : styles.smallInterviewBoxTitle}`}
+      >
+        {title}
+      </div>
+      <div
+        className={`${styles.first_details} ${type == 'big' ? styles.bigDetails : type == 'mid' ? styles.details : styles.smallDetails}`}
+      >
         <div
-          className={styles.first_detailBox}
+          className={`${type == 'big' ? styles.big_first_detailBox : type == 'mid' ? styles.first_detailBox : styles.small_first_detailBox}`}
         >{`현재까지 ${applicant}명 지원`}</div>
 
-        <div className={styles.first_detailBox}>{`${wating}명 승인 대기`}</div>
-        <div className={styles.first_detailBox}>
+        <div
+          className={`${type == 'big' ? styles.big_first_detailBox : type == 'mid' ? styles.first_detailBox : styles.small_first_detailBox}`}
+        >{`${wating}명 승인 대기`}</div>
+        <div
+          className={`${type == 'big' ? styles.big_first_detailBox : type == 'mid' ? styles.first_detailBox : styles.small_first_detailBox}`}
+        >
           {moment(deadline).format('YYYY.MM.DD~')}
         </div>
       </div>
-      <img className={styles.image} src={image} />
-      <div className={`${styles.details} ${styles.second_details}`}>
-        <div className={styles.second_detailBox}>{`${time}분`}</div>
-        <div className={styles.second_detailBox}>{`${cost}원`}</div>
-        <div className={styles.second_detailBox}>{`${way} 선호`}</div>
-        <div className={styles.second_detailBox}>{compensate}</div>
+      <img
+        className={`${type == 'big' ? styles.bigImage : type == 'mid' ? styles.image : styles.smallImage}`}
+        src={image}
+      />
+      <div
+        className={`${styles.second_details} ${type == 'big' ? styles.bigDetails : type == 'mid' ? styles.details : styles.smallDetails}`}
+      >
+        <div
+          className={`${type == 'big' ? styles.big_second_detailBox : type == 'mid' ? styles.second_detailBox : styles.small_second_detailBox}`}
+        >{`${time}분`}</div>
+        <div
+          className={`${type == 'big' ? styles.big_second_detailBox : type == 'mid' ? styles.second_detailBox : styles.small_second_detailBox}`}
+        >{`${cost}원`}</div>
+        <div
+          className={`${type == 'big' ? styles.big_second_detailBox : type == 'mid' ? styles.second_detailBox : styles.small_second_detailBox}`}
+        >{`${way} 선호`}</div>
+        <div
+          className={`${type == 'big' ? styles.big_second_detailBox : type == 'mid' ? styles.second_detailBox : styles.small_second_detailBox}`}
+        >
+          {compensate}
+        </div>
       </div>
     </Link>
   );
