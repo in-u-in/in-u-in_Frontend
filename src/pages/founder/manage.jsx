3e1be4fd1904b1b1ce1bfb1cbec07008d5c04561
sujinRo <88073842+sujinRo@users.noreply.github.com>
2024-08-30 19:52:03 +0900

    feat: #28 데이터 들어오기 전, loader, 데이터 없을 때, 메시지 뜨도록

--- src/pages/founder/manage.jsx
+++ src/pages/founder/manage.jsx
@@ -9,11 +9,12 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { Mousewheel } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { SyncLoader } from 'react-spinners';
 import 'swiper/css';
 
 export default function ManagePage() {
   const { isOpen, openModal, closeModal } = useModal();
-  const [data, setData] = useState();
+  const [data, setData] = useState({ interview: 'loading' });
   const [activeIndex, setActiveIndex] = useState(0);
   const [intervieweeList, setIntervieweeList] = useState([]);
 
@@ -37,64 +38,85 @@ export default function ManagePage() {
   }, [activeIndex]);
 
   return (
-    <div className={styles.container}>
-      <Swiper
-        modules={[Mousewheel]}
-        spaceBetween={0} //아이템 사이 간격
-        slidesPerView={1} //보여질 갯수
-        mousewheel={{
-          enabled: true,
-          forceToAxis: true,
-          sensitivity: 5,
-          thresholdDelta: 1, //한 번 휠에 몇 개 이동
-        }} //축에 맞게 스크롤 적용
-        direction='vertical' //슬라이딩 방향
-        slidesPerGroup={1} //한번에 슬라이딩 될 갯수
-        slidesOffsetBefore={18.08549} //슬라이드 시작 부분 여백
-        onSlideChange={(swiper) => setActiveIndex(swiper.realIndex)} //realIndex: 활성화된 슬라이드 index
-        scrollbar={{
-          draggable: true,
-        }}
-        loop={true}
-        roundLengths={true} //슬라이드 너비 및 높이 값 반올림_흐릿한 텍스트, 이미지 방지 위해
-        className={styles.swiper}
-      >
-        <div className={styles.swiperContainer}>
-          {data?.interview?.map((item, idx) => {
-            return (
-              <SwiperSlide key={idx}>
-                <InterviewBox
-                  type={'big'}
-                  title={item.title}
-                  applicant={item.applicant}
-                  wating={item.wating}
-                  deadline={item.deadline}
-                  image={item.image}
-                  time={item.time}
-                  cost={item.cost}
-                  way={item.way}
-                  compensate={item.compensate}
-                />
-              </SwiperSlide>
-            );
-          })}
+    <>
+      {data?.interview == null ? (
+        <div className={styles.nullContainer}>
+          인터뷰가 없습니다.
+          <br /> 인터뷰를 추가해보세요!
         </div>
-      </Swiper>
-      <div className={styles.intervieweeBox}>
-        {intervieweeList?.map((item, idx) => (
-          <IntervieweeBox
-            key={idx}
-            type={'refuseInterviewee'}
-            profile={item.profile}
-            name={item.name}
-            subject={item.subject}
-            job={item.job}
-            ways={item.ways}
-            openModal={openModal}
-          />
-        ))}
-      </div>
-      {isOpen ? <RefuseModal closeModal={closeModal} /> : <></>}
-    </div>
+      ) : (
+        <>
+          {data.interview == 'loading' ? (
+            <SyncLoader />
+          ) : (
+            <div className={styles.container}>
+              <Swiper
+                modules={[Mousewheel]}
+                spaceBetween={0} //아이템 사이 간격
+                slidesPerView={1} //보여질 갯수
+                mousewheel={{
+                  enabled: true,
+                  forceToAxis: true,
+                  sensitivity: 5,
+                  thresholdDelta: 1, //한 번 휠에 몇 개 이동
+                }} //축에 맞게 스크롤 적용
+                direction='vertical' //슬라이딩 방향
+                slidesPerGroup={1} //한번에 슬라이딩 될 갯수
+                slidesOffsetBefore={18.08549} //슬라이드 시작 부분 여백
+                onSlideChange={(swiper) => setActiveIndex(swiper.realIndex)} //realIndex: 활성화된 슬라이드 index
+                scrollbar={{
+                  draggable: true,
+                }}
+                loop={true}
+                roundLengths={true} //슬라이드 너비 및 높이 값 반올림_흐릿한 텍스트, 이미지 방지 위해
+                className={styles.swiper}
+              >
+                <div className={styles.swiperContainer}>
+                  {data?.interview?.map((item, idx) => {
+                    return (
+                      <SwiperSlide key={idx}>
+                        <InterviewBox
+                          type={'big'}
+                          title={item.title}
+                          applicant={item.applicant}
+                          wating={item.wating}
+                          deadline={item.deadline}
+                          image={item.image}
+                          time={item.time}
+                          cost={item.cost}
+                          way={item.way}
+                          compensate={item.compensate}
+                        />
+                      </SwiperSlide>
+                    );
+                  })}
+                </div>
+              </Swiper>
+              <div className={styles.intervieweeBox}>
+                {intervieweeList == null ? (
+                  <div className={styles.nullIntervieweeBox}>
+                    인터뷰 지원자가 없습니다
+                  </div>
+                ) : (
+                  intervieweeList.map((item, idx) => (
+                    <IntervieweeBox
+                      key={idx}
+                      type={'refuseInterviewee'}
+                      profile={item.profile}
+                      name={item.name}
+                      subject={item.subject}
+                      job={item.job}
+                      ways={item.ways}
+                      openModal={openModal}
+                    />
+                  ))
+                )}
+              </div>
+              {isOpen ? <RefuseModal closeModal={closeModal} /> : <></>}
+            </div>
+          )}
+        </>
+      )}
+    </>
   );
 }
